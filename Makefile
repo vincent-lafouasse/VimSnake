f87397fc3e50d2cc195eead710fcaf6a76050a33
vincent lafouasse <poss@possmbp.home>
2023-09-15 01:36:20 +0200

    SDL hello world

---
+++ Makefile
@@ -0,0 +1,25 @@
+EXEC = VimTrainer
+
+SRC = src/main.c
+
+CC = clang
+CFLAGS = -Wall -Wextra
+# CFLAGS += -Werror
+
+LDLIBS = -lSDL2 -lSDL2_image
+
+run: $(EXEC)
+	./$(EXEC)
+
+$(EXEC): $(SRC)
+	$(CC) $(CFLAGS)  -o $(EXEC) $(SRC) $(LDLIBS)
+
+clean:
+	rm -rf $(EXEC)
+
+re: clean $(EXEC)
+
+fmt:
+	clang-format -i $(shell find src -name '*.c')
+
+.PHONY: run clean re fmt
---
+++ src/main.c
@@ -0,0 +1,78 @@
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_image.h>
+
+#include <stdbool.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+#define HEIGHT 600
+#define WIDTH 800
+#define TARGET_FPS 100
+
+#define NICE_BLUE 33, 118, 174, 255
+
+void cap_fps(uint32_t frame_beginning_tick, int target_fps);
+void init_SDL(SDL_Window **return_window, SDL_Renderer **return_renderer);
+
+int main(void) {
+  SDL_Window *window;
+  SDL_Renderer *renderer;
+  init_SDL(&window, &renderer);
+
+  SDL_Event event;
+  uint32_t frame_beginning_tick;
+  bool running = true;
+
+  while (running) {
+    frame_beginning_tick = SDL_GetTicks();
+    if (SDL_PollEvent(&event)) {
+      if (event.type == SDL_QUIT) {
+        running = false;
+        break;
+      }
+    }
+
+    SDL_SetRenderDrawColor(renderer, NICE_BLUE);
+    SDL_RenderClear(renderer);
+
+    SDL_RenderPresent(renderer);
+
+    cap_fps(frame_beginning_tick, TARGET_FPS);
+  }
+  SDL_DestroyRenderer(renderer);
+  SDL_DestroyWindow(window);
+  SDL_Quit();
+  return (EXIT_SUCCESS);
+}
+void cap_fps(uint32_t frame_beginning_tick, int target_fps) {
+  int ms_per_frame = 1000.0 / target_fps;
+  uint32_t frame_end_tick = SDL_GetTicks();
+  int time_to_wait = ms_per_frame - (frame_end_tick - frame_beginning_tick);
+
+  if (time_to_wait > 0) {
+    SDL_Delay(time_to_wait);
+  }
+}
+
+void init_SDL(SDL_Window **return_window, SDL_Renderer **return_renderer) {
+  const int SCREEN_X_POS = 0;
+  const int SCREEN_Y_POS = 0;
+
+  if (SDL_Init(SDL_INIT_VIDEO) < 0) {
+    printf("SDL could not initialize! SDL_Error: %s\n", SDL_GetError());
+    exit(EXIT_FAILURE);
+  }
+  *return_window = SDL_CreateWindow("a window", SCREEN_X_POS, SCREEN_Y_POS,
+                                    WIDTH, HEIGHT, SDL_WINDOW_OPENGL);
+  if (*return_window == NULL) {
+    SDL_Log("Could not create a window: %s", SDL_GetError());
+    exit(EXIT_FAILURE);
+  }
+  *return_renderer =
+      SDL_CreateRenderer(*return_window, -1, SDL_RENDERER_ACCELERATED);
+  if (*return_renderer == NULL) {
+    SDL_Log("Could not create a renderer: %s", SDL_GetError());
+    exit(EXIT_FAILURE);
+  }
+}
