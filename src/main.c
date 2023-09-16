d3a58ba3ff32f79e4c552bd8e3c1c72287cb928d
vincent lafouasse <poss@possmbp.home>
2023-09-16 17:41:44 +0200

    introduce Sprite struct

--- src/main.c
+++ src/main.c
@@ -13,8 +13,8 @@
 #define NICE_BLUE 33, 118, 174, 255
 
 typedef struct {
-  SDL_Window *window;
-  SDL_Renderer *renderer;
+  SDL_Window* window;
+  SDL_Renderer* renderer;
 } GameRenderer;
 
 GameRenderer init_SDL(void) {
@@ -41,15 +41,48 @@ GameRenderer init_SDL(void) {
   return game_renderer;
 }
 
-void destroy_game_renderer(GameRenderer game_renderer) {
+void free_game_renderer(GameRenderer game_renderer) {
   SDL_DestroyRenderer(game_renderer.renderer);
   SDL_DestroyWindow(game_renderer.window);
+}
+
+typedef struct {
+  SDL_Surface* surface;
+  SDL_Texture* texture;
+  int width;
+  int height;
+} Sprite;
+
+Sprite sprite_from_png(const char* png_path, GameRenderer game_renderer) {
+  Sprite sprite;
+  int width, height;
+
+  sprite.surface = IMG_Load(png_path);
+  sprite.texture =
+      SDL_CreateTextureFromSurface(game_renderer.renderer, sprite.surface);
+  SDL_QueryTexture(sprite.texture, NULL, NULL, &width, &height);
+  sprite.height = height;
+  sprite.width = width;
+
+  return sprite;
+}
+
+void render_sprite(Sprite sprite, int x, int y, GameRenderer game_renderer) {
+  SDL_Rect dst_rect = {x, y, sprite.width, sprite.height};
+  SDL_RenderCopy(game_renderer.renderer, sprite.texture, NULL, &dst_rect);
+}
+
+void free_sprite(Sprite sprite) {
+  SDL_DestroyTexture(sprite.texture);
+  SDL_FreeSurface(sprite.surface);
 }
 
 void cap_fps(uint32_t frame_beginning_tick, int target_fps);
 
 int main(void) {
   GameRenderer game_renderer = init_SDL();
+
+  Sprite crab = sprite_from_png("src/crab.png", game_renderer);
 
   SDL_Event event;
   uint32_t frame_beginning_tick;
@@ -67,12 +100,15 @@ int main(void) {
     SDL_SetRenderDrawColor(game_renderer.renderer, NICE_BLUE);
     SDL_RenderClear(game_renderer.renderer);
 
+    render_sprite(crab, 0, 0, game_renderer);
+
     SDL_RenderPresent(game_renderer.renderer);
 
     cap_fps(frame_beginning_tick, TARGET_FPS);
   }
 
-  destroy_game_renderer(game_renderer);
+  free_sprite(crab);
+  free_game_renderer(game_renderer);
   SDL_Quit();
   return (EXIT_SUCCESS);
 }
