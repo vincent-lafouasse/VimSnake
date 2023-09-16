121fdecd86cbcb614d8785380f2298a29abfc764
vincent lafouasse <poss@possmbp.home>
2023-09-16 17:28:30 +0200

    introduce GameRenderer struct

--- src/main.c
+++ src/main.c
@@ -12,13 +12,44 @@
 
 #define NICE_BLUE 33, 118, 174, 255
 
-void cap_fps(uint32_t frame_beginning_tick, int target_fps);
-void init_SDL(SDL_Window **return_window, SDL_Renderer **return_renderer);
-
-int main(void) {
+typedef struct {
   SDL_Window *window;
   SDL_Renderer *renderer;
-  init_SDL(&window, &renderer);
+} GameRenderer;
+
+GameRenderer init_SDL(void) {
+  GameRenderer game_renderer;
+  const int SCREEN_X_POS = 0;
+  const int SCREEN_Y_POS = 0;
+
+  if (SDL_Init(SDL_INIT_VIDEO) < 0) {
+    printf("SDL could not initialize! SDL_Error: %s\n", SDL_GetError());
+    exit(EXIT_FAILURE);
+  }
+  game_renderer.window = SDL_CreateWindow(
+      "a window", SCREEN_X_POS, SCREEN_Y_POS, WIDTH, HEIGHT, SDL_WINDOW_OPENGL);
+  if (game_renderer.window == NULL) {
+    SDL_Log("Could not create a window: %s", SDL_GetError());
+    exit(EXIT_FAILURE);
+  }
+  game_renderer.renderer =
+      SDL_CreateRenderer(game_renderer.window, -1, SDL_RENDERER_ACCELERATED);
+  if (game_renderer.renderer == NULL) {
+    SDL_Log("Could not create a renderer: %s", SDL_GetError());
+    exit(EXIT_FAILURE);
+  }
+  return game_renderer;
+}
+
+void destroy_game_renderer(GameRenderer game_renderer) {
+  SDL_DestroyRenderer(game_renderer.renderer);
+  SDL_DestroyWindow(game_renderer.window);
+}
+
+void cap_fps(uint32_t frame_beginning_tick, int target_fps);
+
+int main(void) {
+  GameRenderer game_renderer = init_SDL();
 
   SDL_Event event;
   uint32_t frame_beginning_tick;
@@ -33,18 +64,19 @@ int main(void) {
       }
     }
 
-    SDL_SetRenderDrawColor(renderer, NICE_BLUE);
-    SDL_RenderClear(renderer);
+    SDL_SetRenderDrawColor(game_renderer.renderer, NICE_BLUE);
+    SDL_RenderClear(game_renderer.renderer);
 
-    SDL_RenderPresent(renderer);
+    SDL_RenderPresent(game_renderer.renderer);
 
     cap_fps(frame_beginning_tick, TARGET_FPS);
   }
-  SDL_DestroyRenderer(renderer);
-  SDL_DestroyWindow(window);
+
+  destroy_game_renderer(game_renderer);
   SDL_Quit();
   return (EXIT_SUCCESS);
 }
+
 void cap_fps(uint32_t frame_beginning_tick, int target_fps) {
   int ms_per_frame = 1000.0 / target_fps;
   uint32_t frame_end_tick = SDL_GetTicks();
@@ -52,27 +84,5 @@ void cap_fps(uint32_t frame_beginning_tick, int target_fps) {
 
   if (time_to_wait > 0) {
     SDL_Delay(time_to_wait);
-  }
-}
-
-void init_SDL(SDL_Window **return_window, SDL_Renderer **return_renderer) {
-  const int SCREEN_X_POS = 0;
-  const int SCREEN_Y_POS = 0;
-
-  if (SDL_Init(SDL_INIT_VIDEO) < 0) {
-    printf("SDL could not initialize! SDL_Error: %s\n", SDL_GetError());
-    exit(EXIT_FAILURE);
-  }
-  *return_window = SDL_CreateWindow("a window", SCREEN_X_POS, SCREEN_Y_POS,
-                                    WIDTH, HEIGHT, SDL_WINDOW_OPENGL);
-  if (*return_window == NULL) {
-    SDL_Log("Could not create a window: %s", SDL_GetError());
-    exit(EXIT_FAILURE);
-  }
-  *return_renderer =
-      SDL_CreateRenderer(*return_window, -1, SDL_RENDERER_ACCELERATED);
-  if (*return_renderer == NULL) {
-    SDL_Log("Could not create a renderer: %s", SDL_GetError());
-    exit(EXIT_FAILURE);
   }
 }
